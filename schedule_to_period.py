a4085a2d427ddd3e3ba4e8fbcf3468945a0aa48e
Ricson Cheng <ricsonc@andrew.cmu.edu>
2016-02-06 11:34:55 -0500

    outlined more funtions

---
+++ schedule_to_period.py
@@ -0,0 +1,17 @@
+import icalendar
+import sys
+sys.path.append('./scheduler/display_cal/')
+import models
+
+def ics_to_p(filename):
+    '''converts ics to period object'''
+    pass
+
+def event_to_periods(event):
+    '''converts an event to periods'''
+    pass
+
+def schedule_to_p(filename):
+    '''converts from schedule to period obj'''
+    pass
+

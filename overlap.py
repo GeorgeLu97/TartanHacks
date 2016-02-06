5586ebc49964486915f3f7507e03cbfeaec6a8f9
GeorgeLu97 <georgelu97@gmail.com>
2016-02-06 09:35:50 -0500

    More Overlap Stuff

--- overlap.py
+++ overlap.py
@@ -20,12 +20,30 @@ def overlap_schedule(s1, s2):
     #for each e1 from s1 and e2 from s2
     #add their overlap to the schedule
     #return schedule
+    #Don't Mention Time Complexity
     ret = Schedule()
     for component1 in s1.events():
         for component2 in s2.events():
             e = overlap(component1, component2)
-            if not e:
+            if e:
+                e.summary = "Shared With " + s2.Owner
                 ret.events.append(e)
 
     return ret
 
+def overlap_s_schedule(sh1,s1,s2):
+    #returns shared schedule based on s1, with sh1 being already shared
+    r1 = overlap_schedule(s1,s2)
+    for e1 in r1.events():
+        paired = False
+        e = Event()
+        for e2 in sh1.events():
+            if overlap(e1,e2):
+                paired = True
+                e = overlap(e1,e2)
+                break
+        if paired:
+            e.summary = e.summary + " " + s2.Owner
+        else:
+            sh1.append(e1)
+    return sh1

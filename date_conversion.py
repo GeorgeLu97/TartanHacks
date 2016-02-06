e554b4c2238b5973a33e43740aaa36591f36d8bd
Ricson Cheng <ricsonc@andrew.cmu.edu>
2016-02-06 10:54:47 -0500

    updated .gitignore

--- date_conversion.py
+++ date_conversion.py
@@ -4,5 +4,5 @@ def get_dt(hour, day):
     '''goes from hour and day of week to a datetime object'''
     daymap = {'MO':11,'TU':12,'WE':13,'TH':14,'FR':15,'SA':16,'SU':17}
     if hour == 24:
-        hour = 23.9999
+        hour = 23.9999 #can't do 24 hours
     return datetime.datetime(2016,1,daymap[day],int(hour),int(60*(hour-int(hour))))

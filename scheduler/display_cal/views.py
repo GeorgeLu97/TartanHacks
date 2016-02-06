3dc4560fd038f5bd58a549b3117268f235aa19e7
yaochie <yaochisite@gmail.com>
2016-02-06 10:20:25 -0500

    schedule square display

--- scheduler/display_cal/views.py
+++ scheduler/display_cal/views.py
@@ -2,20 +2,30 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.template import loader
 from django.shortcuts import render
-
+import datetime
 from .models import Period, Person, Course
 
 def index(request, user=None):
     template = loader.get_template('display_cal/index.html')
     
     user = Person.objects.filter(username__exact=user).first()
-    periods = []
+    userPeriods = []
+    periods = [datetime.time(h,m) for h in range(7,21) for m in [0,30]]
+    days = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"]
+    
+    schedule = [[(startTime, False) for day in days] for startTime in periods]
     
     if user:
         courses = user.course_set.all()
         for course in courses:
-            periods.extend(list(Period.objects.filter(courses__name__contains=course.name)))
+            userPeriods.extend(list(Period.objects.filter(courses__name__contains=course.name)))
     
-    context = {'days_of_week': ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"],
-               'periods': periods}
+        for period in userPeriods:
+            old = schedule[periods.index(period.startTime)][days.index(period.day)]
+            schedule[periods.index(period.startTime)][days.index(period.day)] = (old[0], True)
+    
+    context = {'days_of_week': days,
+               'user_periods': userPeriods,
+               'periods': periods,
+               'schedule': schedule}
     return render(request, 'display_cal/index.html', context)

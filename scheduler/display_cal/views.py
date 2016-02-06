003a102828208fa7c2aee5a47adb604814c45476
yaochie <yaochisite@gmail.com>
2016-02-05 23:29:34 -0500

    fixed merge

--- scheduler/display_cal/views.py
+++ scheduler/display_cal/views.py
@@ -5,9 +5,5 @@ from django.shortcuts import render
 
 def index(request):
     template = loader.get_template('display_cal/index.html')
-<<<<<<< HEAD
     context = {'days_of_week': ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]}
-=======
-    context = None
->>>>>>> 021fd84b07be709bdfaadaa09c4f095d89b20392
     return render(request, 'display_cal/index.html', context)

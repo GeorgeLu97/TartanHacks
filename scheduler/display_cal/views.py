eefc1533d831d00ee137ff6bfa81cffbfb5f8820
yaochie <yaochisite@gmail.com>
2016-02-05 22:22:04 -0500

    Basic hover

--- scheduler/display_cal/views.py
+++ scheduler/display_cal/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-from django.views import generic
+from django.views.generic.base import TemplateView
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hi")
+    template = loader.get_template('display_cal/index.html')
+    context = None
+    return render(request, 'display_cal/index.html', context)
